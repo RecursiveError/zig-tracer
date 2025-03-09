2e6119fc28cd1790ce7432d7f34a367344ff4e5f
RecursiveError <guilhermesschultz.contato@gmail.com>
2025-03-08 22:35:26 -0300

    update link

--- build.zig
+++ build.zig
@@ -1,38 +1,18 @@
 const std = @import("std");
-const deps = @import("./deps.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
-    const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
+    const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("tracer", .{ .root_source_file = b.path("src/mod.zig") });
+    const extra = b.dependency("zig_extras", .{}).module("extras");
 
-    addTest(b, target, mode, disable_llvm, mod, 0);
-    addTest(b, target, mode, disable_llvm, mod, 1);
-    addTest(b, target, mode, disable_llvm, mod, 2);
-    addTest(b, target, mode, disable_llvm, mod, 3);
 
-    const test_step = b.step("test", "Run all library tests");
-    test_step.dependOn(b.default_step);
-}
-
-fn addTest(b: *std.Build, target: std.Build.ResolvedTarget, mode: std.builtin.Mode, disable_llvm: bool, mod: *std.Build.Module, comptime backend: u8) void {
-    _ = mod;
-    const options = b.addOptions();
-    options.addOption(usize, "src_file_trimlen", std.fs.path.dirname(std.fs.path.dirname(@src().file).?).?.len);
-    options.addOption(u8, "backend", backend);
-
-    const exe = b.addExecutable(.{
-        .name = "test" ++ std.fmt.comptimePrint("{d}", .{backend}),
-        .root_source_file = b.path("src/main.zig"),
+    
+    const trace = b.addModule("zig-tracer", .{
+        .root_source_file = b.path("src/mod.zig"),
         .target = target,
-        .optimize = mode,
+        .optimize = optimize,
     });
-    deps.addAllTo(exe);
-    exe.linkLibC();
-    exe.root_module.addImport("build_options", options.createModule());
-    exe.use_llvm = !disable_llvm;
-    exe.use_lld = !disable_llvm;
-    b.installArtifact(exe);
+
+    trace.addImport("extras", extra);
 }
